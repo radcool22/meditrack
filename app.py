ad56e1af18073c8019ae09a1943ab4c55e782f20
radcool22 <kabirgupta2201@gmail.com>
2024-01-05 15:44:52 +0530

    llama2 trial

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
-# replace this with your OpenAI key
-os.environ["OPENAI_API_KEY"] = "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"
-
 from embedchain import App
-app = App()
+
+os.environ["REPLICATE_AP_TOKEN"] = "r8_IWpChdyOSMpLXUehqeeXLKQSp0fiZUI3s7Eco"
+
+app = App.from_config(config_path="config.yaml")
+
 app.add("https://www.forbes.com/profile/elon-musk")
 app.add("https://en.wikipedia.org/wiki/Elon_Musk")
 app.query("What is the net worth of Elon Musk today?")
